d8d0921aaa56f03bbc4e5325c4b3a61c082a6f99
aziz-hammadi <hammadi.aziz.pro@gmail.com>
2025-11-10 17:08:45 +0100

    correction sur ft_load avec exercice 2

--- 1-Array/ex03/load_image.py
+++ 1-Array/ex03/load_image.py
@@ -9,22 +9,33 @@ def ft_load(path: str) -> npt.NDArray | None:
 
     """
     try:
+        # Charger l'image avec PIL pour obtenir le format
         with Image.open(path) as pil_img:
             img_format = pil_img.format
             print(f"Image format: {img_format}")
+            
+            # Si l'image est en niveaux de gris, la convertir en RGB
+            """if len(img.shape) == 2:
+                img = np.stack((img,)*3, axis=-1)
+            elif img.shape[2] == 4:
+                img = img[:, :, :3]"""
+            # S'assurer que l'image est bien en RGB
             if pil_img.mode != 'RGB':
                 pil_img = pil_img.convert('RGB')
             img = np.array(pil_img)
+            #print(img)
+
             print(f"The shape of image is: {img.shape}")
-            # Aperçu des pixels : 3 premières et 3 dernières lignes
-            print(img[:3])
-            print("...")
-            print(img[-3:])
-            return img
+            # Afficher un aperçu des pixels (3 premières et 3 dernières lignes)
+            #np.set_printoptions(threshold=3, edgeitems=2)
+            ret = img.reshape(1, pil_img.size[0] * pil_img.size[1], 3)
+            print(ret.shape)
+            return ret
+    
     except FileNotFoundError:
         print(f"Erreur : Le fichier {path} n'a pas été trouvé.")
-        return None
+        return
     except Exception as e:
         print(f"Erreur : {e}")
-        return None
+        return
 
